d1bf51da6263cba2d6f2f0c523f290b6a61cbc58
J.P. Hutchins <jphutchins@gmail.com>
2023-07-17 22:29:49 -0700

    feat: namespace cpp magic with _IC_M_

--- Macros/ic-macro-magic/include/ic_macro_magic/ushet/cpp_magic.h
+++ Macros/ic-macro-magic/include/ic_macro_magic/ushet/cpp_magic.h
@@ -17,8 +17,8 @@
  * macro definitions to handle O(N) arguments (with the exception of DEFERn).
  */
 
-#ifndef CPP_MAGIC_H
-#define CPP_MAGIC_H
+#ifndef _IC_M_CPP_MAGIC_H
+#define _IC_M_CPP_MAGIC_H
 
 /**
  * Force the pre-processor to expand the macro a large number of times. Usage:
@@ -31,9 +31,9 @@
  * this example is contrived, this behaviour is abused to implement bounded
  * recursion in macros such as FOR.
  *
- *   #define A(x) x+1
- *   #define EMPTY
- *   #define NOT_QUITE_RIGHT(x) A EMPTY (x)
+ *   #define _IC_M_A(x) x+1
+ *   #define _IC_M_EMPTY
+ *   #define _IC_M_NOT_QUITE_RIGHT(x) A EMPTY (x)
  *   NOT_QUITE_RIGHT(999)
  *
  * Here's what happens inside the C preprocessor:
@@ -58,7 +58,7 @@
  * expands arguments in their own pass. If we define a macro which does nothing
  * except produce its arguments e.g.:
  *
- *   #define PASS_THROUGH(...) __VA_ARGS__
+ *   #define _IC_M_PASS_THROUGH(...) __VA_ARGS__
  *
  * We can now do "PASS_THROUGH(NOT_QUITE_RIGHT(999))" causing "NOT_QUITE_RIGHT" to be
  * expanded to "A (999)", as described above, when the arguments are expanded.
@@ -70,29 +70,29 @@
  * preprocessor making a large number of expansion passes over the given
  * expression.
  */
-#define EVAL(...) EVAL1024(__VA_ARGS__)
-#define EVAL1024(...) EVAL512(EVAL512(__VA_ARGS__))
-#define EVAL512(...) EVAL256(EVAL256(__VA_ARGS__))
-#define EVAL256(...) EVAL128(EVAL128(__VA_ARGS__))
-#define EVAL128(...) EVAL64(EVAL64(__VA_ARGS__))
-#define EVAL64(...) EVAL32(EVAL32(__VA_ARGS__))
-#define EVAL32(...) EVAL16(EVAL16(__VA_ARGS__))
-#define EVAL16(...) EVAL8(EVAL8(__VA_ARGS__))
-#define EVAL8(...) EVAL4(EVAL4(__VA_ARGS__))
-#define EVAL4(...) EVAL2(EVAL2(__VA_ARGS__))
-#define EVAL2(...) EVAL1(EVAL1(__VA_ARGS__))
-#define EVAL1(...) __VA_ARGS__
+#define _IC_M_EVAL(...) EVAL1024(__VA_ARGS__)
+#define _IC_M_EVAL1024(...) EVAL512(EVAL512(__VA_ARGS__))
+#define _IC_M_EVAL512(...) EVAL256(EVAL256(__VA_ARGS__))
+#define _IC_M_EVAL256(...) EVAL128(EVAL128(__VA_ARGS__))
+#define _IC_M_EVAL128(...) EVAL64(EVAL64(__VA_ARGS__))
+#define _IC_M_EVAL64(...) EVAL32(EVAL32(__VA_ARGS__))
+#define _IC_M_EVAL32(...) EVAL16(EVAL16(__VA_ARGS__))
+#define _IC_M_EVAL16(...) EVAL8(EVAL8(__VA_ARGS__))
+#define _IC_M_EVAL8(...) EVAL4(EVAL4(__VA_ARGS__))
+#define _IC_M_EVAL4(...) EVAL2(EVAL2(__VA_ARGS__))
+#define _IC_M_EVAL2(...) EVAL1(EVAL1(__VA_ARGS__))
+#define _IC_M_EVAL1(...) __VA_ARGS__
 
 
 /**
  * Macros which expand to common values
  */
-#define PASS(...) __VA_ARGS__
-#define EMPTY()
-#define COMMA() ,
-#define PLUS() +
-#define ZERO() 0
-#define ONE() 1
+#define _IC_M_PASS(...) __VA_ARGS__
+#define _IC_M_EMPTY()
+#define _IC_M_COMMA() ,
+#define _IC_M_PLUS() +
+#define _IC_M_ZERO() 0
+#define _IC_M_ONE() 1
 
 /**
  * Causes a function-style macro to require an additional pass to be expanded.
@@ -118,7 +118,7 @@
  * 3. At this point the macro expansion completes. If one more pass is made,
  *    IN_NEXT_PASS(args, to, the, macro) will be expanded as desired.
  */
-#define DEFER1(id) id EMPTY()
+#define _IC_M_DEFER1(id) id EMPTY()
 
 /**
  * As with DEFER1 except here n additional passes are required for DEFERn.
@@ -128,32 +128,32 @@
  * Note that there doesn't appear to be a way of combining DEFERn macros in
  * order to achieve exponentially increasing defers e.g. as is done by EVAL.
  */
-#define DEFER2(id) id EMPTY EMPTY()()
-#define DEFER3(id) id EMPTY EMPTY EMPTY()()()
-#define DEFER4(id) id EMPTY EMPTY EMPTY EMPTY()()()()
-#define DEFER5(id) id EMPTY EMPTY EMPTY EMPTY EMPTY()()()()()
-#define DEFER6(id) id EMPTY EMPTY EMPTY EMPTY EMPTY EMPTY()()()()()()
-#define DEFER7(id) id EMPTY EMPTY EMPTY EMPTY EMPTY EMPTY EMPTY()()()()()()()
-#define DEFER8(id) id EMPTY EMPTY EMPTY EMPTY EMPTY EMPTY EMPTY EMPTY()()()()()()()()
+#define _IC_M_DEFER2(id) id EMPTY EMPTY()()
+#define _IC_M_DEFER3(id) id EMPTY EMPTY EMPTY()()()
+#define _IC_M_DEFER4(id) id EMPTY EMPTY EMPTY EMPTY()()()()
+#define _IC_M_DEFER5(id) id EMPTY EMPTY EMPTY EMPTY EMPTY()()()()()
+#define _IC_M_DEFER6(id) id EMPTY EMPTY EMPTY EMPTY EMPTY EMPTY()()()()()()
+#define _IC_M_DEFER7(id) id EMPTY EMPTY EMPTY EMPTY EMPTY EMPTY EMPTY()()()()()()()
+#define _IC_M_DEFER8(id) id EMPTY EMPTY EMPTY EMPTY EMPTY EMPTY EMPTY EMPTY()()()()()()()()
 
 
 /**
  * Indirection around the standard ## concatenation operator. This simply
  * ensures that the arguments are expanded (once) before concatenation.
  */
-#define CAT(a, ...) a ## __VA_ARGS__
-#define CAT3(a, b, ...) a ## b ## __VA_ARGS__
+#define _IC_M_CAT(a, ...) a ## __VA_ARGS__
+#define _IC_M_CAT3(a, b, ...) a ## b ## __VA_ARGS__
 
 
 /**
  * Get the first argument and ignore the rest.
  */
-#define FIRST(a, ...) a
+#define _IC_M_FIRST(a, ...) a
 
 /**
  * Get the second argument and ignore the rest.
  */
-#define SECOND(a, b, ...) b
+#define _IC_M_SECOND(a, b, ...) b
 
 /**
  * Expects a single input (not containing commas). Returns 1 if the input is
@@ -164,8 +164,8 @@
  * This macro abuses the fact that PROBE() contains a comma while other valid
  * inputs must not.
  */
-#define IS_PROBE(...) SECOND(__VA_ARGS__, 0)
-#define PROBE() ~, 1
+#define _IC_M_IS_PROBE(...) SECOND(__VA_ARGS__, 0)
+#define _IC_M_PROBE() ~, 1
 
 
 /**
@@ -176,32 +176,32 @@
  * concatenated string will be produced. IS_PROBE then simply checks to see if
  * the PROBE was returned, cleanly converting the argument into a 1 or 0.
  */
-#define NOT(x) IS_PROBE(CAT(_NOT_, x))
-#define _NOT_0 PROBE()
+#define _IC_M_NOT(x) IS_PROBE(CAT(_NOT_, x))
+#define _IC_M__NOT_0 PROBE()
 
 /**
  * Macro version of C's famous "cast to bool" operator (i.e. !!) which takes
  * anything and casts it to 0 if it is 0 and 1 otherwise.
  */
-#define BOOL(x) NOT(NOT(x))
+#define _IC_M_BOOL(x) NOT(NOT(x))
 
 /**
  * Logical OR. Simply performs a lookup.
  */
-#define OR(a,b) CAT3(_OR_, a, b)
-#define _OR_00 0
-#define _OR_01 1
-#define _OR_10 1
-#define _OR_11 1
+#define _IC_M_OR(a,b) CAT3(_OR_, a, b)
+#define _IC_M__OR_00 0
+#define _IC_M__OR_01 1
+#define _IC_M__OR_10 1
+#define _IC_M__OR_11 1
 
 /**
  * Logical AND. Simply performs a lookup.
  */
-#define AND(a,b) CAT3(_AND_, a, b)
-#define _AND_00 0
-#define _AND_01 0
-#define _AND_10 0
-#define _AND_11 1
+#define _IC_M_AND(a,b) CAT3(_AND_, a, b)
+#define _IC_M__AND_00 0
+#define _IC_M__AND_01 0
+#define _IC_M__AND_10 0
+#define _IC_M__AND_11 1
 
 
 /**
@@ -219,10 +219,10 @@
  * 4. Note that the "true" clause is in the extra set of brackets; thus these
  *    become the arguments to _IF_0 or _IF_1 and thus a selection is made!
  */
-#define IF(c) _IF(BOOL(c))
-#define _IF(c) CAT(_IF_,c)
-#define _IF_0(...)
-#define _IF_1(...) __VA_ARGS__
+#define _IC_M_IF(c) _IF(BOOL(c))
+#define _IC_M__IF(c) CAT(_IF_,c)
+#define _IC_M__IF_0(...)
+#define _IC_M__IF_1(...) __VA_ARGS__
 
 /**
  * Macro if/else statement. Usage:
@@ -234,10 +234,10 @@
  *
  * The mechanism is analogous to IF.
  */
-#define IF_ELSE(c) _IF_ELSE(BOOL(c))
-#define _IF_ELSE(c) CAT(_IF_ELSE_,c)
-#define _IF_ELSE_0(t,f) f
-#define _IF_ELSE_1(t,f) t
+#define _IC_M_IF_ELSE(c) _IF_ELSE(BOOL(c))
+#define _IC_M__IF_ELSE(c) CAT(_IF_ELSE_,c)
+#define _IC_M__IF_ELSE_0(t,f) f
+#define _IC_M__IF_ELSE_1(t,f) t
 
 
 /**
@@ -261,8 +261,8 @@
  * 4. BOOL is used to force non-zero results into 1 giving the clean 0 or 1
  *    output required.
  */
-#define HAS_ARGS(...) BOOL(FIRST(_END_OF_ARGUMENTS_ __VA_ARGS__)(0))
-#define _END_OF_ARGUMENTS_(...) BOOL(FIRST(__VA_ARGS__))
+#define _IC_M_HAS_ARGS(...) BOOL(FIRST(_END_OF_ARGUMENTS_ __VA_ARGS__)(0))
+#define _IC_M__END_OF_ARGUMENTS_(...) BOOL(FIRST(__VA_ARGS__))
 
 
 /**
@@ -274,8 +274,8 @@
  *
  * Example Usage:
  *
- *   #define MAKE_HAPPY(x) happy_##x
- *   #define COMMA() ,
+ *   #define _IC_M_MAKE_HAPPY(x) happy_##x
+ *   #define _IC_M_COMMA() ,
  *   MAP(MAKE_HAPPY, COMMA, 1,2,3)
  *
  * Which expands to:
@@ -329,14 +329,14 @@
  *   _MAP_INNER as expanding to itself and thus it will still be expanded in
  *   future productions of itself.
  */
-#define MAP(...) \
+#define _IC_M_MAP(...) \
    IF(HAS_ARGS(__VA_ARGS__))(EVAL(MAP_INNER(__VA_ARGS__)))
-#define MAP_INNER(op,sep,cur_val, ...) \
+#define _IC_M_MAP_INNER(op,sep,cur_val, ...) \
   op(cur_val) \
   IF(HAS_ARGS(__VA_ARGS__))( \
     sep() DEFER2(_MAP_INNER)()(op, sep, ##__VA_ARGS__) \
   )
-#define _MAP_INNER() MAP_INNER
+#define _IC_M__MAP_INNER() MAP_INNER
 
 
 /**
@@ -352,7 +352,7 @@
  *
  * Example:
  *
- *   #define MAKE_STATIC_VAR(type, name) static type name;
+ *   #define _IC_M_MAKE_STATIC_VAR(type, name) static type name;
  *   MAP_WITH_ID(MAKE_STATIC_VAR, EMPTY, int, int, int, bool, char)
  *
  * Which expands to:
@@ -361,14 +361,14 @@
  *
  * The mechanism is analogous to the MAP macro.
  */
-#define MAP_WITH_ID(op,sep,...) \
+#define _IC_M_MAP_WITH_ID(op,sep,...) \
   IF(HAS_ARGS(__VA_ARGS__))(EVAL(MAP_WITH_ID_INNER(op,sep,I, ##__VA_ARGS__)))
-#define MAP_WITH_ID_INNER(op,sep,id,cur_val, ...) \
+#define _IC_M_MAP_WITH_ID_INNER(op,sep,id,cur_val, ...) \
   op(cur_val,id) \
   IF(HAS_ARGS(__VA_ARGS__))( \
     sep() DEFER2(_MAP_WITH_ID_INNER)()(op, sep, CAT(id,I), ##__VA_ARGS__) \
   )
-#define _MAP_WITH_ID_INNER() MAP_WITH_ID_INNER
+#define _IC_M__MAP_WITH_ID_INNER() MAP_WITH_ID_INNER
 
 
 /**
@@ -382,7 +382,7 @@
  *
  * Example:
  *
- *   #define MAKE_STATIC_VAR(type, name) static type name;
+ *   #define _IC_M_MAKE_STATIC_VAR(type, name) static type name;
  *   MAP_PAIRS(MAKE_STATIC_VAR, EMPTY, char, my_char, int, my_int)
  *
  * Which expands to:
@@ -391,14 +391,14 @@
  *
  * The mechanism is analogous to the MAP macro.
  */
-#define MAP_PAIRS(op,sep,...) \
+#define _IC_M_MAP_PAIRS(op,sep,...) \
   IF(HAS_ARGS(__VA_ARGS__))(EVAL(MAP_PAIRS_INNER(op,sep,__VA_ARGS__)))
-#define MAP_PAIRS_INNER(op,sep,cur_val_1, cur_val_2, ...) \
+#define _IC_M_MAP_PAIRS_INNER(op,sep,cur_val_1, cur_val_2, ...) \
   op(cur_val_1,cur_val_2) \
   IF(HAS_ARGS(__VA_ARGS__))( \
     sep() DEFER2(_MAP_PAIRS_INNER)()(op, sep, __VA_ARGS__) \
   )
-#define _MAP_PAIRS_INNER() MAP_PAIRS_INNER
+#define _IC_M__MAP_PAIRS_INNER() MAP_PAIRS_INNER
 
 /**
  * This is a variant of the MAP macro which iterates over a two-element sliding
@@ -413,9 +413,9 @@
  *
  * Example:
  *
- *   #define SIMON_SAYS_OP(simon, next) IF(NOT(simon()))(next)
- *   #define SIMON_SAYS_LAST_OP(val) last_but_not_least_##val
- *   #define SIMON_SAYS() 0
+ *   #define _IC_M_SIMON_SAYS_OP(simon, next) IF(NOT(simon()))(next)
+ *   #define _IC_M_SIMON_SAYS_LAST_OP(val) last_but_not_least_##val
+ *   #define _IC_M_SIMON_SAYS() 0
  *
  *   MAP_SLIDE(SIMON_SAYS_OP, SIMON_SAYS_LAST_OP, EMPTY, wiggle, SIMON_SAYS, dance, move, SIMON_SAYS, boogie, stop)
  *
@@ -425,21 +425,21 @@
  *
  * The mechanism is analogous to the MAP macro.
  */
-#define MAP_SLIDE(op,last_op,sep,...) \
+#define _IC_M_MAP_SLIDE(op,last_op,sep,...) \
   IF(HAS_ARGS(__VA_ARGS__))(EVAL(MAP_SLIDE_INNER(op,last_op,sep,__VA_ARGS__)))
-#define MAP_SLIDE_INNER(op,last_op,sep,cur_val, ...) \
+#define _IC_M_MAP_SLIDE_INNER(op,last_op,sep,cur_val, ...) \
   IF(HAS_ARGS(__VA_ARGS__))(op(cur_val,FIRST(__VA_ARGS__))) \
   IF(NOT(HAS_ARGS(__VA_ARGS__)))(last_op(cur_val)) \
   IF(HAS_ARGS(__VA_ARGS__))( \
     sep() DEFER2(_MAP_SLIDE_INNER)()(op, last_op, sep, __VA_ARGS__) \
   )
-#define _MAP_SLIDE_INNER() MAP_SLIDE_INNER
+#define _IC_M__MAP_SLIDE_INNER() MAP_SLIDE_INNER
 
 
 /**
  * Strip any excess commas from a set of arguments.
  */
-#define REMOVE_TRAILING_COMMAS(...) \
+#define _IC_M_REMOVE_TRAILING_COMMAS(...) \
 	MAP(PASS, COMMA, __VA_ARGS__)
 
 
