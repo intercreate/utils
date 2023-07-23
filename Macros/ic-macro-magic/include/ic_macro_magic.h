fe784aef65a473bf57d87c5653707c6c14006b7b
J.P. Hutchins <jphutchins@gmail.com>
2023-07-23 16:42:35 -0700

    clang-format

--- Macros/ic-macro-magic/include/ic_macro_magic.h
+++ Macros/ic-macro-magic/include/ic_macro_magic.h
@@ -2,16 +2,16 @@
  * Copyright (c) 2023, Intercreate, Inc.
  *
  * SPDX-License-Identifier: Apache-2.0
- * 
- * uSHET library 
+ *
+ * uSHET library
  *      Copyright (c) 2014 Thomas Nixon, Jonathan Heathcote
- * 
+ *
  *      SPDX-License-Identifier: MIT
- * 
+ *
  * Zephyr Util Macros
  *      Copyright (c) 2011-2014, Wind River Systems, Inc.
  *      Copyright (c) 2021, Nordic Semiconductor ASA
- * 
+ *
  *      SPDX-License-Identifier: Apache-2.0
  */
 
@@ -29,13 +29,13 @@
 
 /**
  * @brief Macro that maps the prefix onto each item of the list, separated by _.
- * 
+ *
  * @details For example, IC_PREFIXES_FOR(MY_NAMESPACE, ONE, TWO, THREE) would produce
  * MY_NAMESPACE_ONE, MY_NAMESPACE_TWO, MY_NAMESPACE_THREE.
- * 
+ *
  * @param prefix The prefix to add to each item of the list
  * @param ... Variable argument list
-*/
+ */
 #define IC_PREFIXES_FOR(prefix, ...) _IC_PREFIXES_FOR(prefix, __VA_ARGS__)
 
 #define IC_FLAGS_FOR(...) _IC_FLAGS_FOR(__VA_ARGS__)
