3609170db900fb2973a5440de375ca1144912409
J.P. Hutchins <jphutchins@gmail.com>
2023-07-23 16:55:17 -0700

    clang-format

--- Macros/ic-macro-magic/tests/test_tests/main.c
+++ Macros/ic-macro-magic/tests/test_tests/main.c
@@ -1,12 +1,12 @@
 /**
  * This file is here to test the unity install and can also be used as a
  * template for new tests.
-*/
+ */
 
 #include "unity.h"
 
-void setUp (void) {} /* Is run before every test, put unit init calls here. */
-void tearDown (void) {} /* Is run after every test, put unit clean-up calls here. */
+void setUp(void) {} /* Is run before every test, put unit init calls here. */
+void tearDown(void) {} /* Is run after every test, put unit clean-up calls here. */
 
 void test(void) {
     TEST_ASSERT_EQUAL(1, 1);
@@ -16,6 +16,6 @@ int main(void) {
     UNITY_BEGIN();
 
     RUN_TEST(test);
-    
+
     return UNITY_END();
 }
