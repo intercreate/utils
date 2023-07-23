6e2bfd730c3d195c93674c662bbe10bb45102022
J.P. Hutchins <jphutchins@gmail.com>
2023-07-23 16:42:48 -0700

    fix: can call test.cmake from parent dir

--- Macros/ic-macro-magic/tests/test.cmake
+++ Macros/ic-macro-magic/tests/test.cmake
@@ -12,17 +12,28 @@
 #   cmake -S test.cmake
 #
 
-execute_process(COMMAND cmake "-Bbuild" RESULT_VARIABLE res)
+execute_process(
+    COMMAND cmake "-Bbuild"
+    RESULT_VARIABLE res 
+    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+)
 if(NOT ${res} STREQUAL "0")
     message(FATAL_ERROR "CMake configuration step failed.")
 endif()
 
-execute_process(COMMAND cmake "--build" "build" "--clean-first" RESULT_VARIABLE res)
+execute_process(
+    COMMAND cmake "--build" "build" "--clean-first"
+    RESULT_VARIABLE res
+    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+)
 if(NOT ${res} STREQUAL "0")
     message(FATAL_ERROR "CMake build step failed.")
 endif()
 
-execute_process(COMMAND ctest "-V" "--test-dir" "build" RESULT_VARIABLE res)
+execute_process(COMMAND ctest "-V" "--test-dir" "build"
+    RESULT_VARIABLE res
+    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+)
 if(NOT ${res} STREQUAL "0")
     message(FATAL_ERROR "1 or more tests failed.")
 endif()
